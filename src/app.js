542c89576f571bb4dfd54ddeccd2a62d411fde05
CHUSGF <chusgf2906@gmail.com>
2022-02-06 23:16:29 +0000

    enviado para corregir

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+  let suits = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
+
+  let getRandom = array => {
+    let i = Math.floor(Math.random() * array.length);
+    return array[i];
+  };
+  let suitsRandom = getRandom(suits);
+  document.getElementById("number").innerHTML = getRandom(values);
+  document.getElementById("header").innerHTML = suitsRandom;
+  document.getElementById("footer").innerHTML = suitsRandom;
+
+  let headerSuit = document.getElementById("header");
+  let footerSuit = document.getElementById("footer");
+
+  if (suitsRandom == "&hearts;" || suitsRandom == "&diams;") {
+    headerSuit.style.color = "red";
+    footerSuit.style.color = "red";
+  }
 };
